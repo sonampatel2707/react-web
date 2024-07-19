ee4e6bf6ff132cd7b7e07cde80c73ae358b71a94
Sonam Patel <sonampatel2707@gmail.com>
2024-07-19 13:20:43 +0530

    updated css

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -1,7 +1,7 @@
 export const data = [
     {
         id:1,
-        userName: "city light",
+        userName: "City of Amazing",
         title: "Amazing City",
         description: "City is every that provide everthing that we need, city is over first montech 77",
         pImag:"https://cdn.pixabay.com/photo/2017/07/19/01/41/clouds-2517653_1280.jpg",
@@ -13,7 +13,7 @@ export const data = [
     },
     {
         id:2,
-        userName: "city",
+        userName: "City of Light",
         title: "Light city",
         description: "City is every that provide everthing that we need, city is over first montech 77",
         pImag:"https://cdn.pixabay.com/photo/2018/05/04/07/55/snow-3373432_1280.jpg",
@@ -25,7 +25,7 @@ export const data = [
     },
     {
         id:3,
-        userName: "city light",
+        userName: "City of Nature",
         title: "Nature City",
         description: "City is every that provide everthing that we need, city is over first montech 77",
         pImag:"https://cdn.pixabay.com/photo/2017/05/02/15/30/streets-2278471_1280.jpg",
@@ -37,8 +37,8 @@ export const data = [
     },
     {
         id:4,
-        userName: "City Light",
-        title: "amazing city",
+        userName: "City of Global",
+        title: "Global City",
         description: "City is every that provide everthing that we need, city is over first montech 77",
         pImag:"https://cdn.pixabay.com/photo/2021/04/06/14/46/city-6156596_1280.jpg",
         mImag:"https://cdn.pixabay.com/photo/2016/10/24/22/43/dubai-1767540_1280.jpg",
@@ -49,7 +49,7 @@ export const data = [
     },
     {
         id:5,
-        userName: "mountain is awesome",
+        userName: "Mountain is Awesome",
         title: "Mountain Beauty",
         description: "Nature is every that provide everthing that we need, city is over first montech ",
         pImag:"https://cdn.pixabay.com/photo/2022/05/09/07/57/bridge-7183945_1280.jpg",
@@ -61,8 +61,8 @@ export const data = [
     },
     {
         id:6,
-        userName: "City Light",
-        title: "amazing city",
+        userName: "City of Smart",
+        title: "Smart City",
         description: "City is every that provide everthing that we need, city is over first montech 77",
         pImag:"https://cdn.pixabay.com/photo/2023/04/22/07/36/night-view-7943221_1280.jpg",
         mImag:"https://cdn.pixabay.com/photo/2017/08/31/05/36/buildings-2699520_1280.jpg",
@@ -73,8 +73,8 @@ export const data = [
     },
     {
         id:7,
-        userName: "City Light",
-        title: "amazing city",
+        userName: "City of Amazing",
+        title: "Amazing City",
         description: "City is every that provide everthing that we need, city is over first montech 77",
         pImag:"https://cdn.pixabay.com/photo/2019/09/11/02/23/buildings-4467663_1280.jpg",
         mImag:"https://cdn.pixabay.com/photo/2019/03/09/13/45/dubai-4044183_1280.jpg",
@@ -85,7 +85,7 @@ export const data = [
     },
     {
         id:8,
-        userName: "night view",
+        userName: "Night of View",
         title: "Night Hill",
         description: "City is every that provide everthing that we need, city is over first montech 77",
         pImag:"https://cdn.pixabay.com/photo/2016/11/19/15/03/buildings-1839726_1280.jpg",
@@ -104,15 +104,15 @@ export const data2 = [
         img:"https://cdn.pixabay.com/photo/2018/11/19/15/06/machang-bridge-3825439_1280.jpg",
         authorImg:"https://cdn.pixabay.com/photo/2019/07/12/18/22/lamps-4333322_1280.jpg",
         authorName:"Paris City",
-        authorText:"City Of light",
+        authorText:"City Of Light",
 
     },
     {
         id:2,
         img:"https://cdn.pixabay.com/photo/2020/01/25/19/13/paris-4793193_1280.jpg",
         authorImg:"https://cdn.pixabay.com/photo/2023/04/22/07/36/night-view-7943221_1280.jpg",
-        authorName:"Night city",
-        authorText:"city of city",
+        authorName:"Night City",
+        authorText:"City Of City",
 
     },
     {
@@ -120,7 +120,7 @@ export const data2 = [
         img:"https://cdn.pixabay.com/photo/2019/02/21/18/36/city-4011922_1280.jpg",
         authorImg:"https://cdn.pixabay.com/photo/2013/03/02/02/41/alley-89197_1280.jpg",
         authorName:"City Space",
-        authorText:"amazing city",
+        authorText:"Amazing City",
 
     },
     {
@@ -128,7 +128,7 @@ export const data2 = [
         img:"https://cdn.pixabay.com/photo/2018/07/16/10/11/table-bay-harbour-3541607_1280.jpg",
         authorImg:"https://cdn.pixabay.com/photo/2023/12/15/14/49/city-8450817_1280.jpg",
         authorName:"London city",
-        authorText:"city of nature",
+        authorText:"City Of Nature",
 
     },
     {
@@ -136,31 +136,31 @@ export const data2 = [
         img:"https://cdn.pixabay.com/photo/2023/04/19/19/11/lake-7938396_1280.jpg",
         authorImg:"https://cdn.pixabay.com/photo/2024/04/28/21/27/moon-8726358_1280.jpg",
         authorName:"Turkey City",
-        authorText:"city of joy",
+        authorText:"City of Joy",
 
     },
     {
         id:6,
         img:"https://cdn.pixabay.com/photo/2024/02/11/15/50/temple-8566784_1280.jpg",
         authorImg:"https://cdn.pixabay.com/photo/2023/04/19/19/11/lake-7938396_1280.jpg",
-        authorName:"Amazing City",
-        authorText:"nature",
+        authorName:"Global City",
+        authorText:"City Of Nature",
 
     },
     {
         id:7,
         img:"https://cdn.pixabay.com/photo/2023/04/06/02/58/castle-7902731_1280.jpg",
         authorImg:"https://cdn.pixabay.com/photo/2023/06/15/20/02/ship-8066436_1280.jpg",
-        authorName:"city",
-        authorText:"nature",
+        authorName:"Twin City",
+        authorText:"Amazing City",
 
     },
     {
         id:8,
         img:"https://cdn.pixabay.com/photo/2016/06/11/13/04/night-1450087_1280.jpg",
         authorImg:"https://cdn.pixabay.com/photo/2023/04/06/02/58/castle-7902731_1280.jpg",
-        authorName:"city",
-        authorText:"nature",
+        authorName:"Light City",
+        authorText:"City Of Light",
 
     }
 ]
